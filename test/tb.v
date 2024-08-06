5c398ede2eef95e51d3b183a36c9b7652b2a4eee
Enjimneering <162901067+Enjimneering@users.noreply.github.com>
2024-08-06 16:32:01 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,5 +1,5 @@
 `default_nettype none
-`timescale 1ns / 1ps
+`timescale 1us / 1ns
 
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
