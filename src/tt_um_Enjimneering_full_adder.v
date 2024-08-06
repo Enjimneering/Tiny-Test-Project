7e3bc022e5b0a1eced3eb622ee142e2f723c3efd
Enjimneering <162901067+Enjimneering@users.noreply.github.com>
2024-08-06 15:52:40 +0100

    Update tt_um_Enjimneering_full_adder.v

--- src/tt_um_Enjimneering_full_adder.v
+++ src/tt_um_Enjimneering_full_adder.v
@@ -1,6 +1,10 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 `default_nettype none
-`include "full_adder.v"
+// `include "full_adder.v"- THIS MAKES IT INCLUDE TWICE, DONT DO THAT
 
 module tt_um_Enjimneering_full_adder (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -14,7 +18,7 @@ module tt_um_Enjimneering_full_adder (
 );
 
 //using full adder
- FullAdder U0 (.a(ui_in[0]), .b(ui_in[1]), .c(ui_in[2]), .carry(uo_out[1]), .sum(uo_out[0]));
+  FullAdder fa (.a(ui_in[0]), .b(ui_in[1]), .c(ui_in[2]), .carry(uo_out[1]), .sum(uo_out[0]));
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
