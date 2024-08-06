115466e183972904c94b8c14bc83521fa4b5f859
Enjimneering <162901067+Enjimneering@users.noreply.github.com>
2024-08-06 16:31:27 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 20, units="ns")
+    clock = Clock(dut.clk, 20, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
